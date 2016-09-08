4f85b83221411cc9101d80aa694c18621f29d338
giorgoch <chatziefstratiougeorgios@gmail.com>
2016-09-08 11:56:13 +0300

    userserviceimpl added

---
+++ src/main/java/com/giorgoch/projectClient/Service/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package com.giorgoch.projectClient.Service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.giorgoch.projectClient.Model.User;
+
+@Service("userSeeviceImpl")
+public class UserServiceImpl  implements UserService{
+
+	@Override
+	public List<User> getUser() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createAcount(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public User registeredNewUserAccount(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> getusers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createXMLFormFormData(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
