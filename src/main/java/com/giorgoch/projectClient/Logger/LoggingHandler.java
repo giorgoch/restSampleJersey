13cf3da490dcbf787e546a21f045922eb961e85e
giorgoch <chatziefstratiougeorgios@gmail.com>
2016-09-10 20:05:28 +0300

    aop loggers added for xml creation method time start- end

--- src/main/java/com/giorgoch/projectClient/Logger/LoggingHandler.java
+++ src/main/java/com/giorgoch/projectClient/Logger/LoggingHandler.java
@@ -1,15 +1,12 @@
 package com.giorgoch.projectClient.Logger;
-
-import java.util.List;
-
+ 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
-import com.giorgoch.projectClient.Model.User;
-
+ 
 @Aspect
 @Component
 public class LoggingHandler {
@@ -19,17 +16,27 @@ public class LoggingHandler {
 	 * 
 	 */
 
-	 Logger log = LoggerFactory.getLogger(getClass());
+	Logger log = LoggerFactory.getLogger(getClass());
 
 	public void loggingUserRegistrationForm() {
 		log.info("registration form user controller");
-		 //System.out.println("registration form user controller");
-	}
-	
-	 
-	public void loggingCreateAccountXML() {
-		 log.info("create account xml ");
 		// System.out.println("registration form user controller");
+	}
+
+	public void loggingCreateAccountXML() {
+		log.info("create account xml method in controller ");
+		// System.out.println("registration form user controller");
+	}
+
+	@Before(value = "execution(* com.giorgoch.projectClient.XMLParsers.Writers.XMLPraserWriterImpl.*(..))")
+	public void loggingcreateXMLFormFormDataStart() {
+		log.info("Start creating xml with form data ");
+	}
+
+	/// projectClient/src/main/java/com/giorgoch/projectClient/XMLParsers/Writers/XMLPraserWriterImpl.java
+	@After(value = "execution(* com.giorgoch.projectClient.XMLParsers.Writers.XMLPraserWriterImpl.*(..))")
+	public void loggingcreateXMLFormFormDataEnd() {
+		log.info("End creating xml wwith form data");
 	}
 
 }
