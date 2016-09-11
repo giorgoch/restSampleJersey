82c8bdab87ccd010a860802d91201777e6abfb1b
giorgoch <chatziefstratiougeorgios@gmail.com>
2016-09-11 11:31:17 +0300

    userdaoimpl finished.TODO tests

--- src/main/java/com/giorgoch/projectClient/Dao/UserDaoImpl.java
+++ src/main/java/com/giorgoch/projectClient/Dao/UserDaoImpl.java
@@ -1,40 +1,92 @@
 package com.giorgoch.projectClient.Dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.jdbc.core.RowMapper;
 
 import com.giorgoch.projectClient.Model.User;
 
+@Component("userdaoImpl")
 public class UserDaoImpl implements UserDao {
+
+	private NamedParameterJdbcTemplate jdbc;
+
+	@Autowired
+	public void setJdbc(NamedParameterJdbcTemplate jdbc) {
+		this.jdbc = jdbc;
+	}
 
 	@Override
 	public List<User> getUsers() {
-		// TODO Auto-generated method stub
-		return null;
+		String sqlGetUser = "Select id,name,lname,username," + "password,postcode,authority,email,"
+				+ "address,dateofbirth,city,country," + "gender " + " from Users";
+		return jdbc.query(sqlGetUser, new RowMapper<User>() {
+			@Override
+			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+				User user = new User();
+				user.setId(rs.getInt("id"));
+				user.setName(rs.getString("name"));
+				user.setLname(rs.getString("lname"));
+				user.setPostcode(rs.getString("postcode"));
+				user.setAddress(rs.getString("address"));
+				user.setCity(rs.getString("city"));
+				user.setTel(rs.getString("tel"));
+				return user;
+			}
+		});
 	}
 
 	@Override
 	public boolean updateUser(User user) {
-		// TODO Auto-generated method stub
-		return false;
+		BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(user);
+		return jdbc.update("update Users set name=:name " + ",lname=:lname," + "postcode=:postcode,"
+				+ "address=:address," + "city=:city," + "tel=:tel", params) == 1;
 	}
 
 	@Override
 	public boolean delete(int id) {
-		// TODO Auto-generated method stub
-		return false;
+		MapSqlParameterSource params = new MapSqlParameterSource();
+		params.addValue("id", id);
+		String sqlDelete = "delete from users where id=:id";
+		return jdbc.update(sqlDelete, params) == 1;
 	}
 
 	@Override
 	public User getUser(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		MapSqlParameterSource params = new MapSqlParameterSource();
+		params.addValue("id", id);
+		String sqlGetUser = "select * from person where id=:id";
+		return jdbc.queryForObject(sqlGetUser, params, new RowMapper<User>() {
+
+			@Override
+			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+				User user = new User();
+				user.setId(rs.getInt("id"));
+				user.setName(rs.getString("name"));
+				user.setLname(rs.getString("lname"));
+				user.setPostcode(rs.getString("postcode"));
+				user.setAddress(rs.getString("address"));
+				user.setCity(rs.getString("city"));
+				user.setTel(rs.getString("tel"));
+				return user;
+			}
+
+		});
 	}
 
 	@Override
 	public boolean create(User user) {
-		// TODO Auto-generated method stub
-		return false;
+		BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(user);
+		String sqlCreate = "insert into users (name,lname,postcode,address,tel,city)"
+				+ "values (:name,:lname,:postcode,:address,:tel,:city)";
+		return jdbc.update(sqlCreate, params) == 1;
 	}
 
-	// TODO unimplemented methods
 }
