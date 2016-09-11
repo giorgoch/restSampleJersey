06da9f873302a5328c2acdc5042ae80a6cda2744
giorgoch <chatziefstratiougeorgios@gmail.com>
2016-09-11 16:23:29 +0300

    aop  pointcut annotations for performance added

--- src/main/java/com/giorgoch/projectClient/Commons/PerformanceLogUtility.java
+++ src/main/java/com/giorgoch/projectClient/Commons/PerformanceLogUtility.java
@@ -4,14 +4,28 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 
 import org.apache.commons.logging.Log;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 
+@Aspect
+@Component
 public class PerformanceLogUtility {
 
+	@Pointcut("execution(* com.giorgoch.projectClient.Dao.UserDaoImpl.*(..))")
+	public void timers() {
+
+	}
+
+	@Before("timers()")
 	public static Long startRecPerformance(Log log, String msg) {
 		log.info(msg);
 		return System.currentTimeMillis();
 	}
 
+	@After("timers()")
 	public static void stoRecPerformance(Log log, Long startTime, String msg) {
 		Long currentTimes = System.currentTimeMillis() - startTime;
 		BigDecimal bigDec = BigDecimal.valueOf(currentTimes);
@@ -19,4 +33,5 @@ public class PerformanceLogUtility {
 		MathContext mc = new MathContext(3);
 		log.info(msg + "[" + bigDec.round(mc) + "sec]");
 	}
+
 }
