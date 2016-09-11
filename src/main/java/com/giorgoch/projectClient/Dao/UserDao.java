37a12de8f4157789cb9feb1252a881593ef6d8d0
giorgoch <chatziefstratiougeorgios@gmail.com>
2016-09-11 11:06:57 +0300

    userdao interface methods added

--- src/main/java/com/giorgoch/projectClient/Dao/UserDao.java
+++ src/main/java/com/giorgoch/projectClient/Dao/UserDao.java
@@ -1,7 +1,18 @@
 package com.giorgoch.projectClient.Dao;
 
+import java.util.List;
+
+import com.giorgoch.projectClient.Model.User;
+
 public interface UserDao {
 
-	
-	//TODO methods
+	public List<User> getUsers();
+
+	public boolean updateUser(User user);
+
+	public boolean delete(int id);
+
+	public User getUser(int id);
+
+	public boolean create(User user);
 }
--- src/main/java/com/giorgoch/projectClient/Dao/UserDaoImpl.java
+++ src/main/java/com/giorgoch/projectClient/Dao/UserDaoImpl.java
@@ -1,6 +1,40 @@
 package com.giorgoch.projectClient.Dao;
 
+import java.util.List;
+
+import com.giorgoch.projectClient.Model.User;
+
 public class UserDaoImpl implements UserDao {
+
+	@Override
+	public List<User> getUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean updateUser(User user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public User getUser(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean create(User user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 	// TODO unimplemented methods
 }
