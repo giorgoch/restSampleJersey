c8100470e6e27e4a414aac8804d66e32c4a58407
giorgoch <chatziefstratiougeorgios@gmail.com>
2016-11-20 22:02:34 +0200

    changes added

--- src/main/java/com/giorgoch/projectClient/Dao/Senders/XMLSenderImpl.java
+++ src/main/java/com/giorgoch/projectClient/Dao/Senders/XMLSenderImpl.java
@@ -1,5 +1,15 @@
 package com.giorgoch.projectClient.Dao.Senders;
 
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
 public class XMLSenderImpl {
+	
+    String sendXmlToUrl="http://localhost:8080/projectClient";
+	CloseableHttpClient httpclient = HttpClients.createDefault();
+	HttpGet httpget = new HttpGet("");
+	CloseableHttpResponse response = httpclient.execute(httpget);
 
 }
