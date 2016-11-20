f8e0f0efa00c3ff119e218c248f5485ca4dd8814
giorgoch <chatziefstratiougeorgios@gmail.com>
2016-11-20 22:59:54 +0200

    adding tools for xml post

--- src/main/java/com/giorgoch/projectClient/Dao/Senders/XmlRendererImpl.java
+++ src/main/java/com/giorgoch/projectClient/Dao/Senders/XmlRendererImpl.java
@@ -1,16 +1,33 @@
 package com.giorgoch.projectClient.Dao.Senders;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.NameValuePair;
 
 public class XmlRendererImpl {
 	
-    String sendXmlToUrl="http://localhost:8080/projectClient";
+    String sendXmlToUrl="http://localhost:8080/projectClient/sendXml";
     
-	CloseableHttpClient httpclient = HttpClients.createDefault();
-	HttpGet httpget = new HttpGet("");
-	CloseableHttpResponse response = httpclient.execute(httpget); 
+    
+    public void sendMethod(){
+		HttpClient client = new DefaultHttpClient();
+    	 HttpPost post = new HttpPost(sendXmlToUrl);
+    	
+    	try{
+    	List<NameValuePair> namedValuePair = new ArrayList<NameValuePair>(1);	
+    	}catch(IOException e){
+    		
+    	}
+    }
+	
 
 }
