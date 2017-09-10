fc2ccdb00bb5e98e84ccbf2b2e3d47cf8377b4a6
giorgoch <chatziefstratiougeorgios@gmail.com>
2017-09-10 19:25:42 +0300

    lombok annotation data added

--- src/main/java/com/giorgoch/projectClient/Model/User.java
+++ src/main/java/com/giorgoch/projectClient/Model/User.java
@@ -4,12 +4,15 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 import org.hibernate.validator.constraints.NotEmpty;
 
+import lombok.Data;
+
+
 /**
  * @author giorgos chatziefstratiou
  *
  */
 @XmlRootElement
-public class User {
+public @Data class User {
 
 	private int id;
 	@NotEmpty
@@ -24,85 +27,9 @@ public class User {
 	private String city;
 	@NotEmpty
 	private String tel;
-
-	public User() {
-
-	}
-
-	public User(int id, String name, String lname, String address, String postcode, String city, String tel) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.lname = lname;
-		this.address = address;
-		this.postcode = postcode;
-		this.city = city;
-		this.tel = tel;
-
-	}
-
-	// get set for user
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getLname() {
-		return lname;
-	}
-
-	public void setLname(String lname) {
-		this.lname = lname;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getPostcode() {
-		return postcode;
-	}
-
-	public void setPostcode(String postcode) {
-		this.postcode = postcode;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getTel() {
-		return tel;
-	}
-
-	public void setTel(String tel) {
-		this.tel = tel;
-	}
-
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", lname=" + lname + ", address=" + address + ", postcode="
-				+ postcode + ", city=" + city + ", tel=" + tel + "]";
-	}
+	
+	
+	
+	
 
 }
