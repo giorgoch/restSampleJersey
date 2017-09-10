0aa59de006e56dcee9eb0fd162a2e6a74d80ebe9
giorgoch <chatziefstratiougeorgios@gmail.com>
2017-09-10 19:26:10 +0300

    comment for lombok

--- src/main/java/com/giorgoch/projectClient/wsService/UserServiceWS.java
+++ src/main/java/com/giorgoch/projectClient/wsService/UserServiceWS.java
@@ -20,10 +20,14 @@ public class UserServiceWS {
 	 * 
 	 */
 	public UserServiceWS() {
-		users.put(1, new User(1, "Alladin", "The Thief", "Persia  3", "12345", "Persia", "123456789"));
-		users.put(2, new User(2, "Lucky", "Loke", "Texas", "74185", "El paso", "000000000"));
-		users.put(3, new User(3, "Marty", "Martian", "Mars 8", "00000", "Mars", "7777777777"));
-		users.put(4, new User(4, "Zeus", "The Olympian", "Mount Olympos", "56789", "Olympos", "99999999"));
+		// users.put(1, new User(1, "Alladin", "The Thief", "Persia 3", "12345",
+		// "Persia", "123456789"));
+		// users.put(2, new User(2, "Lucky", "Loke", "Texas", "74185", "El
+		// paso", "000000000"));
+		// users.put(3, new User(3, "Marty", "Martian", "Mars 8", "00000",
+		// "Mars", "7777777777"));
+		// users.put(4, new User(4, "Zeus", "The Olympian", "Mount Olympos",
+		// "56789", "Olympos", "99999999"));
 	}
 
 	public List<User> getAllUsers() {
@@ -35,8 +39,9 @@ public class UserServiceWS {
 	}
 
 	public User saveUser(User user) {
-		users.put(5, new User(user.getId(), user.getAddress(), user.getCity(), user.getLname(), user.getName(),
-				user.getPostcode(), user.getTel()));
+		// users.put(5, new User(user.getId(), user.getAddress(),
+		// user.getCity(), user.getLname(), user.getName(),user.getPostcode(),
+		// user.getTel()));
 		return user;
 	}
 
