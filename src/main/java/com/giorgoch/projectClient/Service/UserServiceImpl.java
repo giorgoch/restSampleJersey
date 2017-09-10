26a9aa15d808b47dcd84741fd5c8867be896e730
giorgoch <chatziefstratiougeorgios@gmail.com>
2017-09-10 13:23:52 +0300

    adding transactional annotation

--- src/main/java/com/giorgoch/projectClient/Service/UserServiceImpl.java
+++ src/main/java/com/giorgoch/projectClient/Service/UserServiceImpl.java
@@ -3,9 +3,13 @@ package com.giorgoch.projectClient.Service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.giorgoch.projectClient.Model.User;
 
+
+@Transactional(propagation=Propagation.REQUIRED,timeout=50)
 @Service("userServiceImpl")
 public class UserServiceImpl implements UserService {
 
