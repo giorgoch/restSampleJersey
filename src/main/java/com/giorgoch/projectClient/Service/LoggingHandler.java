3c7854078b6df1d060118c81359814ef3a61c778
giorgoch <chatziefstratiougeorgios@gmail.com>
2017-09-10 23:39:24 +0300

    adding empty pointcuts

--- src/main/java/com/giorgoch/projectClient/Service/LoggingHandler.java
+++ src/main/java/com/giorgoch/projectClient/Service/LoggingHandler.java
@@ -1,15 +1,38 @@
 package com.giorgoch.projectClient.Service;
 
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LoggingHandler {
-	
-	
-	Logger log = LoggerFactory.getLogger(this.getClass());
-	
-	
-	
-	
 
+	Logger log = LoggerFactory.getLogger(this.getClass());
+
+	@Pointcut("within(@org.springframework.sterotype.Controller *)")
+	public void controller() {
+
+	}
+
+	@Pointcut("execution(* *.*(..)")
+	public void allMethod(){
+		
+	}
+	
+	@Pointcut("execution(public * *(..))")
+	protected void loggingPublicOperation() {
+	}
+
+	@Pointcut("execution(* *.*(..))")
+	protected void loggingAllOperation() {
+	}
+
+	@Pointcut("within(org.learn.log..*)")
+	private void logAnyFunctionWithinResource() {
+	}
+	
+	
+	
+	
+	
+	
 }
