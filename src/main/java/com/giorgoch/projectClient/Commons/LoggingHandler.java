7dac4143df2ead9ad21be1724ff2e16d151e166b
giorgoch <chatziefstratiougeorgios@gmail.com>
2017-09-11 00:14:03 +0300

    added pointcuts

--- src/main/java/com/giorgoch/projectClient/Commons/LoggingHandler.java
+++ src/main/java/com/giorgoch/projectClient/Commons/LoggingHandler.java
@@ -1,5 +1,12 @@
 package com.giorgoch.projectClient.Commons;
 
+import java.util.Arrays;
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
@@ -15,7 +22,7 @@ public class LoggingHandler {
 	 * 
 	 */
 
-	 Logger log = LoggerFactory.getLogger(getClass());
+	Logger log = LoggerFactory.getLogger(getClass());
 
 	public void loggingUserRegistrationForm() {
 		log.info("registration form user controller");
@@ -34,12 +41,12 @@ public class LoggingHandler {
 
 	@After("loggingXMLFormParserClass()")
 	public void loggingcreateXMLFormFormDataEnd() {
-		//log.info("End creating xml wwith form data");
+		// log.info("End creating xml wwith form data");
 	}
 
 	@Pointcut("execution(* com.giorgoch.projectClient.XMLParsers.Writers.XMLPraserWriterImpl.createXMLFormFormData(..))")
 	public void loggingXMLFormParserClass() {
-	//	log.info("testing the pointcut for xml parser");
+		// log.info("testing the pointcut for xml parser");
 	}
 
 	@Pointcut("execution(* com.giorgoch.projectClient.Resource.UserResource.*())")
@@ -68,5 +75,100 @@ public class LoggingHandler {
 		}
 
 		log.info("Around advice.....after");
+	}
+
+	@Pointcut("within(@org.springframework.stereotype.Controller *)")
+	public void controller() {
+
+	}
+
+	@Pointcut("execution(* *.*(..))")
+	public void allMethod() {
+
+	}
+
+	@Pointcut("execution(public * *(..))")
+	protected void loggingPublicOperation() {
+	}
+
+	@Pointcut("execution(* *.*(..) )")
+	protected void loggingAllOperation() {
+	}
+
+	@Pointcut("within(com.giorgoch.projectClient..*)")
+	private void logAnyFunctionWithinResource() {
+	}
+
+	// before -> Any resource annotated with @Controller annotation
+	// and all method and function taking HttpServletRequest as first parameter
+	@Before("controller() && allMethod() && args(..,request)")
+	public void logBefore(JoinPoint joinPoint, HttpServletRequest request) {
+
+		log.debug("Entering in Method :  " + joinPoint.getSignature().getName());
+		log.debug("Class Name :  " + joinPoint.getSignature().getDeclaringTypeName());
+		log.debug("Arguments :  " + Arrays.toString(joinPoint.getArgs()));
+		log.debug("Target class : " + joinPoint.getTarget().getClass().getName());
+
+		if (null != request) {
+			log.debug("Start Header Section of request ");
+			log.debug("Method Type : " + request.getMethod());
+			Enumeration<String> headerNames = request.getHeaderNames();
+			while (headerNames.hasMoreElements()) {
+				String headerName = headerNames.nextElement();
+				String headerValue = request.getHeader(headerName);
+				log.debug("Header Name: " + headerName + " Header Value : " + headerValue);
+			}
+			log.debug("Request Path info :" + request.getServletPath());
+			log.debug("End Header Section of request ");
+		}
+	}
+
+	// After -> All method within resource annotated with @Controller annotation
+	// and return a value
+	@AfterReturning(pointcut = "controller() && allMethod()", returning = "result")
+	public void logAfter(JoinPoint joinPoint, Object result) {
+		String returnValue = this.getValue(result);
+		log.debug("Method Return value : " + returnValue);
+	}
+
+	// After -> Any method within resource annotated with @Controller annotation
+	// throws an exception ...Log it
+	@AfterThrowing(pointcut = "controller() && allMethod()", throwing = "exception")
+	public void logAfterThrowing(JoinPoint joinPoint, Throwable exception) {
+		log.error("An exception has been thrown in " + joinPoint.getSignature().getName() + " ()");
+		log.error("Cause : " + exception.getCause());
+	}
+
+	// Around -> Any method within resource annotated with @Controller
+	// annotation
+	@Around("controller() && allMethod()")
+	public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+
+		long start = System.currentTimeMillis();
+		try {
+			String className = joinPoint.getSignature().getDeclaringTypeName();
+			String methodName = joinPoint.getSignature().getName();
+			Object result = joinPoint.proceed();
+			long elapsedTime = System.currentTimeMillis() - start;
+			log.debug("Method " + className + "." + methodName + " ()" + " execution time : " + elapsedTime + " ms");
+
+			return result;
+		} catch (IllegalArgumentException e) {
+			log.error("Illegal argument " + Arrays.toString(joinPoint.getArgs()) + " in "
+					+ joinPoint.getSignature().getName() + "()");
+			throw e;
+		}
+	}
+
+	private String getValue(Object result) {
+		String returnValue = null;
+		if (null != result) {
+			if (result.toString().endsWith("@" + Integer.toHexString(result.hashCode()))) {
+				returnValue = ReflectionToStringBuilder.toString(result);
+			} else {
+				returnValue = result.toString();
+			}
+		}
+		return returnValue;
 	}
 }
