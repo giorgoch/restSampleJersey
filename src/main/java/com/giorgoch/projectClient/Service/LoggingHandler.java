3b69a933348e310d60d146f1438e9359a0d59c62
giorgoch <chatziefstratiougeorgios@gmail.com>
2017-09-10 23:27:20 +0300

    logger added

--- src/main/java/com/giorgoch/projectClient/Service/LoggingHandler.java
+++ src/main/java/com/giorgoch/projectClient/Service/LoggingHandler.java
@@ -1,5 +1,15 @@
 package com.giorgoch.projectClient.Service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class LoggingHandler {
+	
+	
+	Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	
+	
+	
 
 }
