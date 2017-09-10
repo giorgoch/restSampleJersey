ec6c878c4dbe3ecef8ad5f485328aadd08becc22
giorgoch <chatziefstratiougeorgios@gmail.com>
2017-09-10 23:41:43 +0300

    modified

--- src/main/java/com/giorgoch/projectClient/Service/LoggingHandler.java
+++ src/main/java/com/giorgoch/projectClient/Service/LoggingHandler.java
@@ -1,5 +1,12 @@
 package com.giorgoch.projectClient.Service;
 
+import java.util.Arrays;
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,10 +21,10 @@ public class LoggingHandler {
 	}
 
 	@Pointcut("execution(* *.*(..)")
-	public void allMethod(){
-		
+	public void allMethod() {
+
 	}
-	
+
 	@Pointcut("execution(public * *(..))")
 	protected void loggingPublicOperation() {
 	}
@@ -29,10 +36,31 @@ public class LoggingHandler {
 	@Pointcut("within(org.learn.log..*)")
 	private void logAnyFunctionWithinResource() {
 	}
+
+	// before -> Any resource annotated with @Controller annotation
+	// and all method and function taking HttpServletRequest as first parameter
+	@Before("controller() && allMethod() && args(..,request)")
+	public void logBefore(JoinPoint joinPoint, HttpServletRequest request) {
+
+		log.debug("Entering in Method :  " + joinPoint.getSignature().getName());
+		log.debug("Class Name :  " + joinPoint.getSignature().getDeclaringTypeName());
+		log.debug("Arguments :  " + Arrays.toString(joinPoint.getArgs()));
+		log.debug("Target class : " + joinPoint.getTarget().getClass().getName());
+
+		if (null != request) {
+			log.debug("Start Header Section of request ");
+			log.debug("Method Type : " + request.getMethod());
+			Enumeration<String> headerNames = request.getHeaderNames();
+			while (headerNames.hasMoreElements()) {
+				String headerName = headerNames.nextElement();
+				String headerValue = request.getHeader(headerName);
+				log.debug("Header Name: " + headerName + " Header Value : " + headerValue);
+			}
+			log.debug("Request Path info :" + request.getServletPath());
+			log.debug("End Header Section of request ");
+		}
+	}
 	
 	
-	
-	
-	
-	
+
 }
